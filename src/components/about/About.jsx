8d06dd3a834a08194ea3172824b1ec9ceefbec3b
mvacjar <mvacjar@gmail.com>
2024-02-29 16:43:53 +0100

    About component added

---
+++ src/components/about/About.jsx
@@ -0,0 +1,15 @@
+import "./about.css";
+
+export default function About() {
+  return (
+    <>
+      <>
+        <div>
+          <article className="footer-container">
+            <h1 className="footer-title">CONTACT ME</h1>
+          </article>
+        </div>
+      </>
+    </>
+  );
+}
