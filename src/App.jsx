ad16605eff2c16377e2e6661c9877f77a9662ce3
mvacjar <mvacjar@gmail.com>
2024-03-02 17:45:41 +0100

    New component added and some minor changes to index.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Header from "./components/header/Header";
 import Projects from "./components/projects/Projects";
 import About from "./components/about/About";
 import Footer from "./components/footer/Footer";
+import ArrowUp from "./components/arrowUp/ArrowUp";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <ArrowUp />
         <Header />
         <Projects />
         <About />
