d0301d9d02b2f1545b87542beeee9b2f9c88ce93
mvacjar <mvacjar@gmail.com>
2024-03-02 17:42:04 +0100

    Arrow Scroll Upp Added

---
+++ src/components/arrowUp/ArrowUp.jsx
@@ -0,0 +1,16 @@
+import arrowUp from "../../assets/images/arrow_up.png";
+import "./arrowUp.css";
+
+import { HashLink as Link } from "react-router-hash-link";
+
+export default function ArrowUp() {
+  return (
+    <>
+      <figure>
+        <Link to="/#Header" smooth={true} duration={500}>
+          <img className="arrow-up" src={arrowUp} alt="arrow scroll up"></img>
+        </Link>
+      </figure>
+    </>
+  );
+}
