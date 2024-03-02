c5b908600e7aa5a2008b939442cff99d25a21cc0
mvacjar <mvacjar@gmail.com>
2024-03-02 17:43:49 +0100

    About me section with animation added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,8 +6,32 @@ export default function About() {
     <>
       <>
         <div>
-          <article className="" id="About">
-            <h1 className="">ABOUT ME</h1>
+          <article className="about-container" id="About">
+            <div className="marquee-container">
+              <div className="marquee-footer-wrapper">
+                <h1 className="marquee-footer-title">SO, WHAT ABOUT ME?</h1>
+              </div>
+              <div className="marquee-footer-wrapper">
+                <h1 className="marquee-footer-title">SO, WHAT ABOUT ME?</h1>
+              </div>
+            </div>
+            <div className="card-container">
+              <section className="card-section">
+                <div className="words-animation">
+                  <p className="word-left bigger">FRONTEND DEVELOPER 👩‍💻</p>
+                  <p className="word-right">Creative Thinker</p>
+                  <p className="word-left">PASSION#</p>
+                  <p className="word-right">Self-teach</p>
+                  <p className="word-left bigger">💪 NEW CHALLENGES</p>
+                  <p className="word-right">Communicative</p>
+                  <p className="word-left">TEAM PLAYER</p>
+                  <p className="word-right">Problem Solver</p>
+                  <p className="word-left bigger">#INNOVATIVE</p>
+                  <p className="word-right">Sharp-minded 🦈</p>
+                  <p className="word-left">AGILE#</p>
+                </div>
+              </section>
+            </div>
           </article>
         </div>
       </>
